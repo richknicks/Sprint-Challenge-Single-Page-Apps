0e38f87ad233c42d41b0bd0c6ddeccac193580e8
richknicks <rlynch@gmail.com>
2020-01-31 12:05:47 -0500

    add axios

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -7,6 +7,14 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then(response =>{
+      console.log(response.data.results);
+      setData(response.data.results);
+    })
+    .catch(error =>{
+      console.log("The data was not returned", error);
+    } )
   }, []);
 
   return (
