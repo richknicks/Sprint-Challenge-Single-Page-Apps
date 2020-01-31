64b6c62e8a9fc1c05acc152f49444c9c4d5a0101
richknicks <rlynch@gmail.com>
2020-01-31 12:02:58 -0500

    add useState

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter]=useState();
+  const [query, setQuery] = useState("");
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
